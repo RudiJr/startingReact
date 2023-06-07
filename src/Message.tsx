12e7e20e82f1c37918e3ab5733c319b9762297e5
RudiJr <rudi.juniior@gmail.com>
2023-06-06 21:34:00 -0300

    practicing react

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing = capitalize the first letter of every word
+function Message (){
+    const name = 'Rudi';
+    if (name)
+    return <h1>Hello {name}</h1>   // JSX = Javascript XML
+    return <h1>Hello World</h1> 
+}   
+
+export default Message;
+ 
